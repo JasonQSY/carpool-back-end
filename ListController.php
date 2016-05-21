0815539cf5a666498e19b1ae492bb7ab8cdc0f53
songqun <songqun.001@163.com>
2016-05-22 02:43:29 +0800

    Add files via upload

---
+++ ListController.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JasonQSY
+ * Date: 5/20/16
+ * Time: 7:20 PM
+ */
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+//use App\Http\Requests\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Input;
+use App\JsonGeneral;
+use Illuminate\Support\Facades\Redirect;
+
+final class ListController extends Controller
+{
+    private $userController;
+    private $jsonGeneral;
+
+    public function __construct(UserController $userController, JsonGeneral $jsonGeneral)
+    {
+        $this->userController = $userController;
+        $this->jsonGeneral = $jsonGeneral;
+    }
+
+    /**
+     * 获取所有活跃的订单
+     *
+     * API: /list/get
+     */
+    public function index()
+    {
+        $returnList = [];
+        $list = DB::select('select * from act WHERE state = ?', [0]);
+        foreach ($list as $item) {
+            $creator = $this->userController->get_username_by_uid($item->creator_uid);
+            $people = [];
+            if ($item->people1_uid !== -1) {
+                $people[] = $this->userController->get_username_by_uid($item->people1_uid);
+            }
+            if ($item->people2_uid !== -1) {
+                $people[] = $this->userController->get_username_by_uid($item->people2_uid);
+            }
+            if ($item->people3_uid !== -1) {
+                $people[] = $this->userController->get_username_by_uid($item->people3_uid);;
+            }
+            $returnList[] = [
+                'creator' => $creator,
+                'name' => $item->name,
+                'people' => $people,
+                'from' => $item->from,
+                'to' => $item->to,
+                'expectedNum' => $item->expectedNumber,
+                'state' => $item->state
+            ];
+        }
+
+        // 其实response应该封装进lib,但是先算了吧
+        return response()->json($returnList);
+    }
+
+    /**
+     * 获取一个订单的具体信息
+     *
+     * @param $id
+     */
+    public function detail($id)
+    {
+        $list = DB::select('select * from act WHERE act_id = ?', [$id]);
+        $item = $list[0];
+        $creator = $this->userController->get_username_by_uid($item->creator_uid);
+        $people = [];
+        if ($item->people1_uid !== -1) {
+            $people[] = $this->userController->get_username_by_uid($item->people1_uid);
+        }
+        if ($item->people2_uid !== -1) {
+            $people[] = $this->userController->get_username_by_uid($item->people2_uid);
+        }
+        if ($item->people3_uid !== -1) {
+            $people[] = $this->userController->get_username_by_uid($item->people3_uid);;
+        }
+        $returnItem = [
+            'creator' => $creator,
+            'name' => $item->name,
+            'people' => $people,
+            'from' => $item->from,
+            'to' => $item->to,
+            'expectedNum' => $item->expectedNumber,
+            'state' => $item->state
+        ];
+
+        // 其实response应该封装进lib,但是先算了吧
+        return response()->json($returnItem);
+    }
+
+    /**
+     * 创建一个订单
+     *
+     * @param info
+     * @return bool->status
+     */
+    public function add()
+    {
+        //$creator = Input::get('creator');
+
+        //$state = Input::get('state');
+        //$people[] = Input::get('people');
+
+        $creator_wechat_openid = Input::get('creator');
+        $name = Input::get('name');
+        $from = Input::get('from');
+        $to = Input::get('to');
+        $expectedNumber = Input::get('expectedNum');
+        if ($this->userController->get_uid_by_wechat_id($creator_wechat_openid)) {
+            try {
+                DB::table('act')->insert([
+                    'name' => $name,
+                    'creator_uid' => $this->userController->get_uid_by_wechat_id($creator_wechat_openid),
+                    'people1_uid' => -1,
+                    'people2_uid' => -1,
+                    'people3_uid' => -1,
+                    'from' => $from,
+                    'to' => $to,
+                    'expectedNumber' => $expectedNumber,
+                    'state' => 0,
+                ]);
+                return $this->jsonGeneral->show_success();  //Use redirect and session ?
+            } catch (Exception $e) {
+                return $this->jsonGeneral->show_error("Database error");
+            }
+        } else {
+            return $this->jsonGeneral->show_error("Invalid wechat_id");
+        }
+
+
+    }
+
+    /**
+     * 更新一个订单
+     * @todo
+     * @param $id
+     * @param $info
+     */
+    public function creatorUpdate($id)  //Post
+    {
+        $list = DB::select('select * from act WHERE act_id = ?', [$id]);
+        $item = $list[0];
+        $name = Input::get('name');
+        $from = Input::get('from');
+        $to = Input::get('to');
+        $expectedNumber = Input::get('expectedNumber');
+        if ($name) {
+            DB::table('act')->where('id',$id)->update(['name' => $name]);
+        }
+        if ($from) {
+            DB::table('act')->where('id',$id)->update(['from' => $from]);
+        }
+        if ($to) {
+            DB::table('act')->where('id',$id)->update(['to' => $to]);
+        }
+        if ($expectedNumber >= $item->expectedNumber){
+            DB::table('act')->where('id',$id)->update(['expectedNumber' => $expectedNumber]);
+        }
+    }
+
+    public  function peopleDropout($id, $people_uid)
+    {
+        $list = DB::select('select * from act WHERE act_id = ?', [$id]);
+        $item = $list[0];
+        switch ($people_uid) {
+            case $item->people1_uid :
+                DB::table('act')->where('id',$id)->update(['people1_uid' => -1]);
+                break;
+            case $item->people2_uid :
+                DB::table('act')->where('id',$id)->update(['people2_uid' => -1]);
+                break;
+            case $item->people3_uid :
+                DB::table('act')->where('id',$id)->update(['people3_uid' => -1]);
+                break;
+            default :
+                return $this->jsonGeneral->show_error("Invalid User");
+        }
+        
+
+    }
+
+    /**
+     * 移除一个订单
+     *
+     * 刚开始有什么好移除的吗,我懒得写了
+     *
+     * @todo
+     * @param $id
+     * @return status
+     */
+    public function remove($id)
+    {
+        // remove according to id.
+        try {
+            $res = DB::delete('delete from act WHERE act_id = ?', [$id]);  //The other act_id(s) will not change
+        } catch (Exception $e) {
+            return $this->jsonGeneral->show_error("Database error");
+        }
+        if ($res) {
+            return $this->jsonGeneral->show_success(); //Use redirect and sessions?
+        } else {
+            return $this->jsonGeneral->show_error("Invalid act id");
+        }
+    }
+}
